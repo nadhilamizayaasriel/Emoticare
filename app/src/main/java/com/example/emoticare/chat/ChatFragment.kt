cd2ddb342438fa9bb88f8acb043730c5177683a0
alweismiau <keizafina@gmail.com>
2024-06-13 06:40:19 +0700

    integrasi chatbot

--- app/src/main/java/com/example/emoticare/chat/ChatFragment.kt
+++ app/src/main/java/com/example/emoticare/chat/ChatFragment.kt
@@ -8,61 +8,20 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.ImageButton
-import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.emoticare.R
 import com.example.emoticare.data.api.ApiConfig
 import com.example.emoticare.data.api.ChatAdapter
 import com.example.emoticare.data.api.ChatResponse
-import kotlinx.coroutines.launch
 import okhttp3.MediaType.Companion.toMediaType
-import okhttp3.MediaType.Companion.toMediaTypeOrNull
-import okhttp3.MultipartBody
-import okhttp3.RequestBody
 import okhttp3.RequestBody.Companion.toRequestBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-//
+
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
-//
-//class ChatFragment : Fragment() {
-//    // TODO: Rename and change types of parameters
-//    private var param1: String? = null
-//    private var param2: String? = null
-//    private var responseListener: ChatResponseListener? = null
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        arguments?.let {
-//            param1 = it.getString(ARG_PARAM1)
-//            param2 = it.getString(ARG_PARAM2)
-//        }
-//    }
-//
-//    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        // Inflate the layout for this fragment
-//        return inflater.inflate(R.layout.fragment_chat, container, false)
-//    }
-//
-//    companion object {
-//        @JvmStatic
-//        fun newInstance(param1: String, param2: String) =
-//            ChatFragment().apply {
-//                arguments = Bundle().apply {
-//                    putString(ARG_PARAM1, param1)
-//                    putString(ARG_PARAM2, param2)
-//                }
-//            }
-//    }
-//}
-
-
 class ChatFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
@@ -84,7 +43,6 @@ class ChatFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_chat, container, false)
     }
 
@@ -108,29 +66,6 @@ class ChatFragment : Fragment() {
             }
         }
     }
-
-//    private fun sendMessage(message: String) {
-//        val requestBody = RequestBody.create("text/plain".toMediaTypeOrNull(), message)
-//        val call = ApiConfig.getApiService().sendChatResponse(requestBody)
-//        call.enqueue(object : Callback<ChatResponse> {
-//            override fun onResponse(call: Call<ChatResponse>, response: Response<ChatResponse>) {
-//                if (response.isSuccessful) {
-//                    val chatResponse = response.body()
-//                    chatResponse?.let {
-//                        it.response?.let { it1 -> updateChat(it1) }
-//                    }
-//                } else {
-//                    // Handle the case where the server responds with an error status.
-//                    Log.e("ChatFragment", "Response error: ${response.errorBody()?.string()}")
-//                }
-//            }
-//
-//            override fun onFailure(call: Call<ChatResponse>, t: Throwable) {
-//                // Handle the case where the call fails, e.g., due to network error.
-//                Log.e("ChatFragment", "Network error: ${t.message}")
-//            }
-//        })
-//    }
 
     private fun sendMessage(message: String) {
         val mediaType = "application/json; charset=utf-8".toMediaType()
