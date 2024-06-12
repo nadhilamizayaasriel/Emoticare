14e9c6712c9713a0dd06ce38f108d0c81a39581c
alweismiau <keizafina@gmail.com>
2024-06-12 23:29:19 +0700

    chatbot

--- app/src/main/java/com/example/emoticare/login/LoginActivity.kt
+++ app/src/main/java/com/example/emoticare/login/LoginActivity.kt
@@ -1,16 +1,27 @@
 package com.example.emoticare.login
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.emoticare.MainActivity
 import com.example.emoticare.R
+import com.example.emoticare.home.HomeActivity
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var btnLogin: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_login)
+
+
+        btnLogin = findViewById(R.id.login_button)
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
+}
